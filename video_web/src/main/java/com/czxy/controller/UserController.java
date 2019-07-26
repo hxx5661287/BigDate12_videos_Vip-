c81ebf7843945f73c7102cc9ccc8c39dfc76a5af
wangaoqi <550894211@qq.com>
2019-07-26 11:33:19 +0800

    王傲旗,杨涵,李思缘成品

---
+++ video_web/src/main/java/com/czxy/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.czxy.controller;
+
+/**
+ * @author 550894211@qq.com
+ * @version v 1.0
+ * @date 2019/7/26
+ */
+
+public class UserController {
+
+    public static void main(String[] args) {
+        System.out.println(1111);
+    }
+}
