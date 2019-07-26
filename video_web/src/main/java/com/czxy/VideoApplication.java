f40b63bbd4796fa285b9193261cdaace4c85006b
wangaoqi <550894211@qq.com>
2019-07-26 11:31:32 +0800

    王傲旗,杨涵,李思缘成品

---
+++ video_web/src/main/java/com/czxy/VideoApplication.java
@@ -0,0 +1,17 @@
+package com.czxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 550894211@qq.com
+ * @version v 1.0
+ * @date 2019/7/26
+ */
+@SpringBootApplication
+public class VideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoApplication.class, args);
+    }
+}
