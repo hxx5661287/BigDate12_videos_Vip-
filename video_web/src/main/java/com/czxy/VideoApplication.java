48dff287162ba7883b6c8a3be23dcd333d71d83e
wangaoqi <550894211@qq.com>
2019-07-26 11:38:03 +0800

    更新一下,改了一下pom文件

--- video_web/src/main/java/com/czxy/VideoApplication.java
+++ video_web/src/main/java/com/czxy/VideoApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class VideoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VideoApplication.class, args);
     }
